fb8e7c6c9f71517a4f22c956d6bf0a2a5148f343
Tim <tim_24398@gmx.de>
2020-04-29 12:52:01 +0200

    Added threading and main

--- lab1/info_clientserver.py
+++ lab1/info_clientserver.py
@@ -1,29 +1,31 @@
-import logging
-import socket
+import logging, socket, threading
 
 import constCS
-from context import lab_logging
+#from context import lab_logging
 
-lab_logging.setup(stream_level=logging.INFO)  # init loging channels for the lab
+#lab_logging.setup(stream_level=logging.INFO)  # init loging channels for the lab
 
 GET_MSG_SIZE = 15
 SIZE_MSG_SIZE = 10
 ENTRY_SIZE = 40
 
-class infoServer:
+class infoServer(threading.Thread):
     _logger = logging.getLogger("vs2lab.lab1.architecture.server")
     _serving = True
     telefondatenbank = \
         {"Pascal": "018054646",
          "Fabian": "01571234",
          "Konstantin": "01751234",
-         "Tim": "49294"}
+         "Tim": "49294",
+         "Alexa": "29042020"}
 
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super(infoServer, self).__init__(*args, **kwargs)
+        self._stopped = threading.Event()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((constCS.HOST, constCS.PORT))
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # prevents errors due to "addresses in use"
+        self.sock.bind((constCS.HOST, constCS.PORT))
         self.sock.settimeout(3)  # time out in order not to block forever
         self._logger.info("Server bound to socket " + str(self.sock))
         print("server started")
@@ -33,6 +35,7 @@ class infoServer:
         while self._serving:  # as long as _serving (checked after connections or socket timeouts)
             try:
                 (connection, address) = self.sock.accept()  # returns new socket and address of client
+
                 while True:  # forever
                     data = connection.recv(GET_MSG_SIZE)  # receive data from client
                     if not data:
@@ -43,11 +46,26 @@ class infoServer:
                     elif msg.startswith("GET"):   # case: return specific entry
                         name = msg.split()[1]
                         self.get(name, connection)
+
                 connection.close()  # close the connection
             except socket.timeout:
                 pass  # ignore timeouts
+
+            if self.isStopped():
+                self.sock.close()
+                self._logger.info("Server down.")
+                return
+
         self.sock.close()
         self._logger.info("Server down.")
+
+    def stop(self):
+        print("setting stop")
+        self._stopped.set()
+
+    def isStopped(self):
+        print("isStoppped")
+        return self._stopped.is_set()
 
     def get(self, name, connection):
         num = self.telefondatenbank.get(name)
@@ -108,3 +126,19 @@ def padding(existingMsg, size):
     while len(existingMsg) < size:  #for i in range(len(msg), size) ?
         existingMsg += " "
     return existingMsg
+
+
+if __name__ == "__main__":
+    print("---Main")
+    # Create server in thread
+    server = infoServer()
+    serverThread = threading.Thread(target=server.serve)
+    serverThread.start()
+
+    # Create client
+    client = clientInterface()
+    result = client.get("Pascal")
+    print(result)
+
+    client.close()
+    server.stop()
